54fe223376b34ea11bc52f3472c946abe996b01c
p32929 <p32929@yahoo.com>
2023-06-18 22:44:46 +0600

    added usm

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import React from 'react'
 import { Button } from "@/components/ui/button";
+import { controller } from '@/lib/StatesController';
+import { useSelector } from 'react-redux';
 
+interface Props {
 
-export default function App() {
-  return (
-    <Button>
-      Hello
-    </Button>
-  )
 }
+
+const App: React.FC<Props> = (props) => {
+  // Hooks
+  const states = useSelector(() => controller.states);
+
+  return (
+    <div>
+      <h1>Counter: {states.counter}</h1>
+      <Button onClick={() => {
+        controller.increase()
+      }}>+++</Button>
+      <Button onClick={() => {
+        controller.decrease()
+      }}>---</Button>
+    </div>
+  )
+
+}
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    tsDecorators: true
+  })],
   server: {
     port: 3000,
   },
